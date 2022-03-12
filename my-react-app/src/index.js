23c2c89a72c4bc03629128a58c992d02c8803d6e
Carson <carsoncolgateccu@gmail.com>
2022-03-11 22:57:31 -0700

    first commit

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from './App';
 
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <HashRouter>
+            {/* //  <BrowserRouter> */}
+            <App />
+            {/* </BrowserRouter> */}
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
